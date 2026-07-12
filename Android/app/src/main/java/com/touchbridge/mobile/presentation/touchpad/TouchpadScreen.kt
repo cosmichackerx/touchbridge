4af36a6a0c0267bdd4364bfa0575acf7a55babe3
cosmichackerx <cosmichackerx@users.noreply.github.com>
2026-07-12 18:51:31 +0500

    Fix Android CI build: launcher icon and touchpad imports.

--- Android/app/src/main/java/com/touchbridge/mobile/presentation/touchpad/TouchpadScreen.kt
+++ Android/app/src/main/java/com/touchbridge/mobile/presentation/touchpad/TouchpadScreen.kt
@@ -39,7 +39,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.awaitPointerEventScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
